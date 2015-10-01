a061a031a566b49e567a8fed0967cbd3b0b13bb4
Scott Patten <scopatgames@gmail.com>
2015-10-01 17:20:03 -0600

    reset to default persistent data on new stage load

--- titleScripts/LoadScene.cs
+++ titleScripts/LoadScene.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class LoadScene : MonoBehaviour {
 
 	public int sceneToLoad;
+	public bool resetDefaults;
 
 	private SceneFadeInOut sceneFadeInOut;
 
 	public void OnEnable () {
 		if (GameObject.FindGameObjectWithTag(Tags.gameController)){
 			sceneFadeInOut = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<SceneFadeInOut> ();
+			if(resetDefaults){
+				GameObject.FindGameObjectWithTag(Tags.mainCamera).GetComponent<PersistentData>().ResetDefaults();
+			}
 			sceneFadeInOut.LoadScene(sceneToLoad);
 		}
 		else{
